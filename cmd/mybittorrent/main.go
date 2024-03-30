ee532bfb684abafde53170d461ccce27a26683d0
Om Budhiraja <ombudhiraja144@gmail.com>
2024-03-31 01:00:43 +0530

    get peers ip from tracker url

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -5,13 +5,72 @@ import (
 	// "encoding/json"
 	"bufio"
 	"crypto/sha1"
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"net/url"
 	"os"
 	"strings"
 
 	"github.com/codecrafters-io/bittorrent-starter-go/internal/bencode"
 )
+
+type TorrentInfo struct {
+	TrackerUrl  string
+	InfoHash    []byte
+	Length      int
+	PieceLength int
+	PieceHashes []string
+}
+
+func (t *TorrentInfo) Print() {
+	fmt.Println("Tracker URL:", t.TrackerUrl)
+	fmt.Println("Length:", t.Length)
+	fmt.Printf("Info Hash: %x\n", t.InfoHash)
+	fmt.Println("Piece Length:", t.PieceLength)
+	fmt.Println("Piece Hashes:")
+
+	for _, pieceHash := range t.PieceHashes {
+		fmt.Println(pieceHash)
+	}
+}
+
+func NewTorrentInfo(reader *bufio.Reader) (*TorrentInfo, error) {
+	res, err := bencode.Decode(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	decoded, ok := res.(map[string]interface{})
+
+	if !ok {
+		return nil, fmt.Errorf("failed to convert decoded value to dictionary")
+	}
+
+	info, ok := decoded["info"].(map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("no info dictionary found in torrent file")
+	}
+
+	sha1Hash := sha1.New()
+	sha1Hash.Write([]byte(bencode.Encode(info)))
+
+	pieceHashes := []string{}
+	pieceHashesBytes := []byte(info["pieces"].(string))
+
+	for i := 0; i < len(pieceHashesBytes); i += 20 {
+		pieceHashes = append(pieceHashes, fmt.Sprintf("%x", pieceHashesBytes[i:i+20]))
+	}
+
+	return &TorrentInfo{
+		TrackerUrl:  decoded["announce"].(string),
+		InfoHash:    sha1Hash.Sum(nil),
+		Length:      info["length"].(int),
+		PieceLength: info["piece length"].(int),
+		PieceHashes: pieceHashes,
+	}, nil
+}
 
 func main() {
 
@@ -55,40 +114,84 @@ func main() {
 
 		reader := bufio.NewReader(file)
 
-		res, err := bencode.Decode(reader)
+		torrentInfo, err := NewTorrentInfo(reader)
+
 		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		decoded, ok := res.(map[string]interface{})
-
-		if !ok {
-			fmt.Println("Failed to convert decoded value to dictionary")
+			fmt.Println("Failed to parse torrent file: " + err.Error())
 			os.Exit(1)
 		}
 
-		info, ok := decoded["info"].(map[string]interface{})
+		torrentInfo.Print()
 
-		if !ok {
-			fmt.Println("No info dictionary found in torrent file")
+	case "peers":
+		if len(os.Args) < 3 {
+			fmt.Println("No torrent file provided")
 			os.Exit(1)
 		}
 
-		// convert info dictionary to sha256 hash
-		sha1Hash := sha1.New()
-		sha1Hash.Write([]byte(bencode.Encode(info)))
+		torrentFile := os.Args[2]
+		file, err := os.Open(torrentFile)
 
-		fmt.Println("Tracker URL:", decoded["announce"])
-		fmt.Println("Length:", info["length"])
-		fmt.Printf("Info Hash: %x\n", sha1Hash.Sum(nil))
-		fmt.Println("Piece Length:", info["piece length"])
-		fmt.Println("Piece Hashes")
+		if err != nil {
+			fmt.Println("Failed to open torrent file: " + err.Error())
+			os.Exit(1)
+		}
 
-		// print 20 byte hashes
-		pieceHashes := []byte(info["pieces"].(string))
-		for i := 0; i < len(pieceHashes); i += 20 {
-			fmt.Printf("%x\n", pieceHashes[i:i+20])
+		reader := bufio.NewReader(file)
+
+		torrentInfo, err := NewTorrentInfo(reader)
+
+		if err != nil {
+			fmt.Println("Failed to parse torrent file: " + err.Error())
+			os.Exit(1)
+		}
+
+		params := url.Values{}
+
+		params.Add("info_hash", string(torrentInfo.InfoHash))
+		params.Add("peer_id", "00112233445566778899")
+		params.Add("port", "6881")
+		params.Add("uploaded", "0")
+		params.Add("downloaded", "0")
+		params.Add("left", fmt.Sprintf("%d", torrentInfo.Length))
+		params.Add("compact", "1")
+
+		resp, err := http.Get(fmt.Sprintf("%s?%s", torrentInfo.TrackerUrl, params.Encode()))
+
+		if err != nil {
+			fmt.Println("Failed to get peers from tracker: " + err.Error())
+			os.Exit(1)
+		}
+
+		defer resp.Body.Close()
+
+		trackerResponseRaw, err := bencode.Decode(bufio.NewReader(resp.Body))
+
+		if err != nil {
+			fmt.Println("Failed to decode peers response: " + err.Error())
+			os.Exit(1)
+		}
+
+		trackerResponse, ok := trackerResponseRaw.(map[string]interface{})
+
+		if !ok {
+			fmt.Println("Failed to convert tracker response to dictionary")
+			os.Exit(1)
+		}
+
+		peers, ok := trackerResponse["peers"].(string)
+
+		if !ok {
+			fmt.Println("Failed to convert peers to string")
+			os.Exit(1)
+		}
+
+		peersBytes := []byte(peers)
+
+		for i := 0; i < len(peersBytes); i += 6 {
+			ip := fmt.Sprintf("%d.%d.%d.%d", peersBytes[i], peersBytes[i+1], peersBytes[i+2], peersBytes[i+3])
+			port := binary.BigEndian.Uint16(peersBytes[i+4 : i+6])
+			fmt.Printf("%s:%d\n", ip, port)
 		}
 
 	default:
