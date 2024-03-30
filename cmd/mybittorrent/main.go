f68de91e234e214b203dd9b031e10dac22eceb3d
Om Budhiraja <ombudhiraja144@gmail.com>
2024-03-30 16:39:30 +0530

    decode lists

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -3,57 +3,118 @@ package main
 import (
 	// Uncomment this line to pass the first stage
 	// "encoding/json"
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"unicode"
-	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
-// Example:
-// - 5:hello -> hello
-// - 10:hello12345 -> hello12345
-func decodeBencode(bencodedString string) (interface{}, error) {
+func decodeString(reader *bufio.Reader) (string, error) {
 
-	if unicode.IsDigit(rune(bencodedString[0])) {
-		var firstColonIndex int
+	// unread the first byte to read the length of the string
+	err := reader.UnreadByte()
 
-		for i := 0; i < len(bencodedString); i++ {
-			if bencodedString[i] == ':' {
-				firstColonIndex = i
-				break
-			}
-		}
-
-		lengthStr := bencodedString[:firstColonIndex]
-
-		length, err := strconv.Atoi(lengthStr)
-		if err != nil {
-			return "", err
-		}
-
-		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
-	} else if bencodedString[0] == 'i' {
-		var endIndex int
-
-		for i := 1; i < len(bencodedString); i++ {
-			if bencodedString[i] == 'e' {
-				endIndex = i
-				break
-			}
-		}
-
-		decodedInteger, err := strconv.Atoi(bencodedString[1:endIndex])
-
-		if err != nil {
-			return "", fmt.Errorf("failed to decode integer: %v", err)
-		}
-
-		return decodedInteger, nil
+	if err != nil {
+		return "", fmt.Errorf("failed to unread first byte for string: %v", err)
 	}
 
-	return "", fmt.Errorf("unsupported bencode type")
+	n, err := reader.ReadBytes(':')
+
+	if err != nil {
+		return "", fmt.Errorf("failed to read length of string: %v", err)
+	}
+
+	length, err := strconv.Atoi(string(n[:len(n)-1]))
+
+	if err != nil {
+		return "", fmt.Errorf("failed to convert length to integer: %v", err)
+	}
+
+	bencodedString := make([]byte, length)
+
+	_, err = reader.Read(bencodedString)
+
+	if err != nil {
+		return "", fmt.Errorf("failed to read string: %v", err)
+	}
+
+	if len(bencodedString) != length {
+		return "", fmt.Errorf("string length mismatch: expected %d, got %d", length, len(bencodedString))
+	}
+
+	return string(bencodedString), nil
+}
+
+func decodeInteger(reader *bufio.Reader) (int, error) {
+
+	intStr, err := reader.ReadBytes('e')
+
+	if err != nil {
+		return 0, fmt.Errorf("failed to read integer: %v", err)
+	}
+
+	integer, err := strconv.Atoi(string(intStr[:len(intStr)-1]))
+
+	if err != nil {
+		return 0, fmt.Errorf("failed to convert integer: %v", err)
+	}
+
+	return integer, nil
+}
+
+func decodeList(reader *bufio.Reader) ([]interface{}, error) {
+
+	var list []interface{}
+
+	for {
+		nextChar, err := reader.Peek(1)
+
+		if err != nil {
+			return nil, fmt.Errorf("failed to peek next byte: %v", err)
+		}
+
+		if nextChar[0] == 'e' {
+			_, err := reader.ReadByte()
+
+			if err != nil {
+				return nil, fmt.Errorf("failed to read list end: %v", err)
+			}
+
+			return list, nil
+		}
+
+		decoded, err := decodeBencode(reader)
+
+		if err != nil {
+			return nil, fmt.Errorf("failed to decode list element: %v", err)
+		}
+
+		list = append(list, decoded)
+
+	}
+}
+
+func decodeBencode(reader *bufio.Reader) (interface{}, error) {
+
+	firstChar, err := reader.ReadByte()
+
+	if err != nil {
+		return "", fmt.Errorf("failed to read first byte: %v", err)
+	}
+
+	switch {
+	case unicode.IsDigit(rune(firstChar)):
+		return decodeString(reader)
+	case firstChar == 'i':
+		return decodeInteger(reader)
+	case firstChar == 'l':
+		return decodeList(reader)
+	default:
+		return "", fmt.Errorf("unsupported bencode type with byte: %s", string(firstChar))
+	}
 }
 
 func main() {
@@ -63,8 +124,9 @@ func main() {
 	if command == "decode" {
 
 		bencodedValue := os.Args[2]
+		reader := bufio.NewReader(strings.NewReader(bencodedValue))
 
-		decoded, err := decodeBencode(bencodedValue)
+		decoded, err := decodeBencode(reader)
 		if err != nil {
 			fmt.Println(err)
 			return
