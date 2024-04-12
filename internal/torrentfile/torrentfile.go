9c5ff566d3f7bcdb31a190fea4c58a5d142aa329
Om Budhiraja <ombudhiraja144@gmail.com>
2024-04-12 16:26:22 +0530

    correctly calculate info hash

--- internal/torrentfile/torrentfile.go
+++ internal/torrentfile/torrentfile.go
@@ -26,11 +26,18 @@ type bencodeInfo struct {
 	Pieces      string `bencode:"pieces"`
 	PieceLength int    `bencode:"piece length"`
 	Length      int    `bencode:"length"`
+	Files       []File `bencode:"files"`
+}
+
+type File struct {
+	Length int      `bencode:"length"`
+	Path   []string `bencode:"path"`
 }
 
 type bencodeTorrent struct {
 	Announce string      `bencode:"announce"`
 	Info     bencodeInfo `bencode:"info"`
+	file     *os.File
 }
 
 func New(path string) (*TorrentFile, error) {
@@ -42,7 +49,9 @@ func New(path string) (*TorrentFile, error) {
 
 	defer file.Close()
 
-	bencodeTo := bencodeTorrent{}
+	bencodeTo := bencodeTorrent{
+		file: file,
+	}
 
 	err = bencode.Unmarshal(file, &bencodeTo)
 
@@ -50,7 +59,7 @@ func New(path string) (*TorrentFile, error) {
 		return nil, err
 	}
 
-	infoHash, err := bencodeTo.Info.hash()
+	infoHash, err := bencodeTo.hash()
 
 	if err != nil {
 		return nil, err
@@ -60,6 +69,14 @@ func New(path string) (*TorrentFile, error) {
 
 	if err != nil {
 		return nil, err
+	}
+
+	isMultiFile := len(bencodeTo.Info.Files) > 0
+
+	if isMultiFile {
+		for _, file := range bencodeTo.Info.Files {
+			bencodeTo.Info.Length += file.Length
+		}
 	}
 
 	return &TorrentFile{
@@ -120,9 +137,31 @@ func (t *TorrentFile) String() string {
 	return sb.String()
 }
 
-func (info *bencodeInfo) hash() ([20]byte, error) {
+func (bt *bencodeTorrent) hash() ([20]byte, error) {
+
+	bt.file.Seek(0, 0)
+
+	decodedTorrent, err := bencode.Decode(bt.file)
+
+	if err != nil {
+		return [20]byte{}, err
+	}
+
+	torrentDict, ok := decodedTorrent.(map[string]interface{})
+
+	if !ok {
+		return [20]byte{}, fmt.Errorf("invalid bencode format")
+	}
+
+	infoDict, ok := torrentDict["info"]
+
+	if !ok {
+		return [20]byte{}, fmt.Errorf("info key not found")
+	}
+
 	var buffer bytes.Buffer
-	err := bencode.Marshal(&buffer, *info)
+	err = bencode.Marshal(&buffer, infoDict)
+
 	if err != nil {
 		return [20]byte{}, err
 	}
